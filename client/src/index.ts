b35a3810bd9cf7bf4a14273ba49cd60884989213
Conor McCarter <conormccarter@gmail.com>
2026-08-29 10:17:55 -0400

    sdk: consolidate npm package entry points

--- client/src/index.ts
+++ client/src/index.ts
@@ -3,3 +3,4 @@ export type { BaseClientOptions, BaseRequestOptions } from "./BaseClient.js";
 export { LoonFSClient } from "./Client.js";
 export { LoonFSError, LoonFSTimeoutError } from "./errors/index.js";
 export * from "./exports.js";
+export * from "./transfers.js";
